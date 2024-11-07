e5e66a2d2a835b7018ba22038bf37a9c88305bff
egokb <egokeyboard@gmail.com>
2024-11-08 00:01:17 +0800

    simplify binding template

--- Packages/com.fujisheng.fui/Runtime/Core/Extensions/BindingContextExtensions.cs
+++ Packages/com.fujisheng.fui/Runtime/Core/Extensions/BindingContextExtensions.cs
@@ -1,32 +1,44 @@
 using FUI.Bindable;
 
+using UnityEngine.UIElements;
+
 namespace FUI.Extensions
 {
     public static class BindingContextExtensions
     {
-        public static void BindingList(IElement element, INotifyCollectionChanged preValue, INotifyCollectionChanged @value)
+        /// <summary>
+        /// 绑定一个可观察列表到Element
+        /// </summary>
+        /// <param name="element">视觉元素</param>
+        /// <param name="preValue">修改前的可观察列表</param>
+        /// <param name="value">修改后的可观察列表</param>
+        public static void BindingList<TElement>(TElement element, INotifyCollectionChanged preValue, INotifyCollectionChanged @value) where TElement : IListView
         {
-            if (element is FUI.IListView listView)
+            if (preValue != null)
             {
-                if (preValue != null)
-                {
-                    preValue.CollectionAdd -= listView.OnAdd;
-                    preValue.CollectionRemove -= listView.OnRemove;
-                    preValue.CollectionReplace -= listView.OnReplace;
-                    preValue.CollectionUpdate -= listView.OnUpdate;
-                }
+                preValue.CollectionAdd -= element.OnAdd;
+                preValue.CollectionRemove -= element.OnRemove;
+                preValue.CollectionReplace -= element.OnReplace;
+                preValue.CollectionUpdate -= element.OnUpdate;
+            }
 
-                if (@value != null)
-                {
-                    @value.CollectionAdd += listView.OnAdd;
-                    @value.CollectionRemove += listView.OnRemove;
-                    @value.CollectionReplace += listView.OnReplace;
-                    @value.CollectionUpdate += listView.OnUpdate;
-                }
+            if (@value != null)
+            {
+                @value.CollectionAdd += element.OnAdd;
+                @value.CollectionRemove += element.OnRemove;
+                @value.CollectionReplace += element.OnReplace;
+                @value.CollectionUpdate += element.OnUpdate;
             }
         }
 
-        public static void UnbindingList<TElement>(this BindingContext context, INotifyCollectionChanged list, string elementPath) where TElement : class, IElement
+        /// <summary>
+        /// 解绑可观察列表和视觉元素
+        /// </summary>
+        /// <typeparam name="TElement">视觉元素类型</typeparam>
+        /// <param name="context"></param>
+        /// <param name="list"></param>
+        /// <param name="elementPath"></param>
+        public static void UnbindingList<TElement>(this BindingContext context, INotifyCollectionChanged list, string elementPath) where TElement : class, IListView
         {
             if (list == null)
             {
@@ -34,13 +46,10 @@ namespace FUI.Extensions
             }
 
             var element = context.View.GetElement<TElement>(elementPath);
-            if (element is FUI.IListView listView)
-            {
-                list.CollectionAdd -= listView.OnAdd;
-                list.CollectionRemove -= listView.OnRemove;
-                list.CollectionReplace -= listView.OnReplace;
-                list.CollectionUpdate -= listView.OnUpdate;
-            }
+            list.CollectionAdd -= element.OnAdd;
+            list.CollectionRemove -= element.OnRemove;
+            list.CollectionReplace -= element.OnReplace;
+            list.CollectionUpdate -= element.OnUpdate;
         }
     }
 }
